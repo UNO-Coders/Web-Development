d21d13887ba9a6a6e4a8c37b334c7366fc744129
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2025-10-12 19:57:31 +0530

    Add files via upload
    A simple blackjack game using html css and js

---
+++ blackjack-game-main/README.md
@@ -0,0 +1,26 @@
+# BlackJack-Game Website
+
+## Description
+This is a simple web application that helps in playing blackjack. It was created by following a tutorial from FreeCodeCamp.
+
+## Features
+- **Start a game**: Allows users to pick a card.
+- **Get a new card**: Users can get a new card and if the total exceeds 21 they are out of the game.
+- **Reset functionality**: Resets the count back to zero.
+
+## How to Use
+1. Open the website.
+2. Click the 'Start Game' button to start the game and get a card.
+3. Use the 'New Card' button to get a new card.
+4. If the total points exceed 21 the user is out of the game.
+
+## Built With
+- HTML
+- CSS
+- JavaScript
+
+## Acknowledgements
+- This project was built by following a FreeCodeCamp tutorial.
+
+## Author
+- Anant Jain
---
+++ blackjack-game-main/script.js
@@ -0,0 +1,58 @@
+let cards=[];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+
+function getRandomCard(){
+    let randomNumber= Math.floor(Math.random()*13)+1;    
+    if(randomNumber===1){
+        return 11;
+    }
+    else if(randomNumber>10){
+        return 10;
+    }
+    else{
+        return randomNumber;
+    }
+}
+
+
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for (let i=0;i<cards.length;i++){
+        cardsEl.textContent+=cards[i]+" ";
+    }
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
+function startGame(){
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
+}
+
+function newCard() {
+    if(isAlive===true&&hasBlackJack===false){
+    let card=getRandomCard();
+    cards.push(card);
+    sum+=card;
+    renderGame();
+}
+}
